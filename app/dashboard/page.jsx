fd783ebd31c63e89df98b68c363a32167d76db33
Mihail Gurin <mike.gurin21@gmail.com>
2023-01-25 22:02:23 +0200

    ..........

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -4,9 +4,7 @@ import Chat from "./components/dashboard/chat/Chat";
 import Upcomming from "./components/dashboard/upcomming/Upcomming";
 
 export const getCurrentUser = async () => {
-  const data = await fetch("https://be-better.netlify.app/api/getCurrentUser", {
-    cache: "no-store",
-  });
+  const data = await fetch("https://be-better.netlify.app/api/getCurrentUser");
   const currentUser = await data.json();
   return currentUser;
 };
--- pages/api/getCurrentUser.js
+++ pages/api/getCurrentUser.js
@@ -1,11 +1,11 @@
 import { auth } from "../../lib/firebase";
 
 export default async function handler(req, res) {
-  const currentUser = await auth.currentUser;
+  const currentUser = auth.currentUser;
   const user = {
-    uid: currentUser?.uid,
-    displayName: currentUser?.displayName,
-    email: currentUser?.email,
+    uid: currentUser.uid,
+    displayName: currentUser.displayName,
+    email: currentUser.email,
   };
 
   res.status(200).json(user);
