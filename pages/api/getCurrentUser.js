8d6c434597cfcb00a344b84e01aa6c9e5753c820
Mihail Gurin <mike.gurin21@gmail.com>
2023-01-25 23:19:44 +0200

    handling adding message udefined current user v8

--- pages/api/getCurrentUser.js
+++ pages/api/getCurrentUser.js
@@ -1,20 +1,13 @@
-import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../lib/firebase";
 
 export default async function handler(req, res) {
-  await onAuthStateChanged(auth, (e) => {
-    if (e) {
-      res.status(200).json(e);
-    } else {
-      res.status(500).json({ message: "User not found" });
-    }
-  });
+  const currentUser = auth.currentUser;
 
-  // const currentUser = auth.currentUser;
+  const user = {
+    uid: currentUser.uid,
+    displayName: currentUser.displayName,
+    email: currentUser.email,
+  };
 
-  // const user = {
-  //   uid: currentUser.uid,
-  //   displayName: currentUser.displayName,
-  //   email: currentUser.email,
-  // };
+  res.status(200).json(user);
 }
