d6dbf37c0ada51c34455bf475855503d063c08b5
Mihail Gurin <mike.gurin21@gmail.com>
2023-01-25 22:31:44 +0200

    handling adding message udefined current user v7

--- app/dashboard/chat/Chats.jsx
+++ app/dashboard/chat/Chats.jsx
@@ -2,7 +2,9 @@ import { getAllChats } from "../../../lib/db";
 import Chat from "./Chat";
 
 export const getCurrentUser = async () => {
-  const data = await fetch("https://be-better.netlify.app/api/getCurrentUser");
+  const data = await fetch("https://be-better.netlify.app/api/getCurrentUser", {
+    cache: "no-store",
+  });
   const currentUser = await data.json();
   return currentUser;
 };
