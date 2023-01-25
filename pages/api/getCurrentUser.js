c53b9572cbf6b08f8b0cc57f0687f84cb83454d6
Mihail Gurin <mike.gurin21@gmail.com>
2023-01-25 22:11:40 +0200

    handling adding message udefined current user v2

--- pages/api/getCurrentUser.js
+++ pages/api/getCurrentUser.js
@@ -3,9 +3,9 @@ import { auth } from "../../lib/firebase";
 export default async function handler(req, res) {
   const currentUser = auth.currentUser;
   const user = {
-    uid: currentUser.uid,
-    displayName: currentUser.displayName,
-    email: currentUser.email,
+    uid: currentUser?.uid,
+    displayName: currentUser?.displayName,
+    email: currentUser?.email,
   };
 
   res.status(200).json(user);
