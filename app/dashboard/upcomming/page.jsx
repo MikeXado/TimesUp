86e06fcfcdaaaa4ce2c7850f8ff3d19e481bbe3b
Mihail Gurin <mike.gurin21@gmail.com>
2023-01-25 22:13:51 +0200

    handling adding message udefined current user v3

--- app/dashboard/upcomming/page.jsx
+++ app/dashboard/upcomming/page.jsx
@@ -1,6 +1,5 @@
 import { getSessions } from "../../../lib/db";
 import TableSessions from "./Table";
-import { useStore } from "../../../utils/store";
 export const getCurrentUser = async () => {
   const data = await fetch("https://be-better.netlify.app/api/getCurrentUser");
   const currentUser = await data.json();
