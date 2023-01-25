304531e8162381c63bc68d1899abe37cfe062b12
Mihail Gurin <mike.gurin21@gmail.com>
2023-01-25 18:26:13 +0200

    added https to fetch url

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -4,7 +4,7 @@ import Chat from "./components/dashboard/chat/Chat";
 import Upcomming from "./components/dashboard/upcomming/Upcomming";
 
 export const getCurrentUser = async () => {
-  const data = await fetch("http://be-better.netlify.app/api/getCurrentUser", {
+  const data = await fetch("https://be-better.netlify.app/api/getCurrentUser", {
     cache: "no-store",
   });
   const currentUser = await data.json();
