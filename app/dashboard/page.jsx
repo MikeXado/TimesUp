f589703ba19ecda55004797313d920025f0cee0a
Mihail Gurin <mike.gurin21@gmail.com>
2023-01-25 21:47:47 +0200

    ........

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -4,7 +4,7 @@ import Chat from "./components/dashboard/chat/Chat";
 import Upcomming from "./components/dashboard/upcomming/Upcomming";
 
 export const getCurrentUser = async () => {
-  const data = await fetch("http://localhost:3000/api/getCurrentUser", {
+  const data = await fetch("https://be-better.netlify.app/api/getCurrentUser", {
     cache: "no-store",
   });
   const currentUser = await data.json();
