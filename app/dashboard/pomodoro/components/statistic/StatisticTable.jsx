0a8fd506346336ed3074528a49df2d7d0f9f8aaf
Mihail Gurin <mike.gurin21@gmail.com>
2023-03-09 18:54:09 +0200

    deleted relative style on statistic table

--- app/dashboard/pomodoro/components/statistic/StatisticTable.jsx
+++ app/dashboard/pomodoro/components/statistic/StatisticTable.jsx
@@ -11,7 +11,7 @@ export default function StatisticTable({ pomodoros }) {
   };
 
   return (
-    <div className="relative flex flex-col min-w-0 break-words  bg-[#111c44] w-full mb-6 shadow-lg rounded">
+    <div className="flex flex-col min-w-0 break-words  bg-[#111c44] w-full mb-6 shadow-lg rounded">
       <div className="rounded-t mb-0 px-4 py-3 border-0">
         <div className="flex flex-wrap items-center">
           <div className="relative w-full px-4 max-w-full flex-grow flex-1">
