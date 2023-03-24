04c5162e2c8b3cabf8ae5e455474ed09f010d002
Gurin Mihail <73294844+MikeXado@users.noreply.github.com>
2023-03-24 18:39:49 +0200

    Updated / route , added new line

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="text-white">
       <Link href="/signIn">Sign In</Link>
+      <div className="text-red-500">There will be a landing page , now it is in development</div>
     </div>
   );
 }
