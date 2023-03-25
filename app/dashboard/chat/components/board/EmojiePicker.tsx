55c4fbf00649e510dac3d68b0d005cf5e1aeeaaf
Mihail Gurin <mike.gurin21@gmail.com>
2023-03-25 15:54:11 +0200

    issue #7

---
+++ app/dashboard/chat/components/board/EmojiePicker.tsx
@@ -0,0 +1,19 @@
+import Picker from "@emoji-mart/react";
+import useSWR from "swr";
+export default function EmojiePicker({ setMessage }) {
+  const getEmojies = async () => {
+    const res = await fetch("https://cdn.jsdelivr.net/npm/@emoji-mart/data");
+    const emojies = await res.json();
+    return emojies;
+  };
+
+  const { data, isLoading } = useSWR("/api/emojies", getEmojies);
+  return (
+    <div className="-top-[450px] -left-[300px]   absolute">
+      <Picker
+        data={data && data}
+        onEmojiSelect={(emojie) => setMessage((prev) => prev + emojie.native)}
+      />{" "}
+    </div>
+  );
+}
