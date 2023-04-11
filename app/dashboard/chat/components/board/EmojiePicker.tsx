6e1f3cfd0ed8f3d263b8da8b4b292d14d9d0455f
Mihail Gurin <mike.gurin21@gmail.com>
2023-04-11 13:29:20 +0300

    fixed issue#17

--- app/dashboard/chat/components/board/EmojiePicker.tsx
+++ app/dashboard/chat/components/board/EmojiePicker.tsx
@@ -1,6 +1,6 @@
 import Picker from "@emoji-mart/react";
 import useSWR from "swr";
-export default function EmojiePicker({ setMessage }) {
+export default function EmojiePicker({ setMessage , closeEmojiePicker }) {
   const getEmojies = async () => {
     const res = await fetch("https://cdn.jsdelivr.net/npm/@emoji-mart/data");
     const emojies = await res.json();
@@ -9,13 +9,14 @@ export default function EmojiePicker({ setMessage }) {
 
   const { data } = useSWR("/api/emojies", getEmojies);
   return (
-    <div className="-top-[450px] left-[300px] absolute">
+    <div className="-top-[450px] left-[0px] absolute">
       <Picker
         data={data}
+        onClickOutside={() => closeEmojiePicker(false)}
         onEmojiSelect={(emojie) =>
           setMessage((prev) => {
             return {
-              value: prev.value + emojie.native,
+              value: prev?.value ? prev.value + emojie.native : emojie.native,
               type: "text",
             };
           })
