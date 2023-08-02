e291ccc977a3a3ba53f15d25f0cdfe573fbe98df
MikeXado <mike.gurin21@gmail.com>
2023-08-02 16:55:10 +0300

    show to users that gpt tab functionality will come soon

--- app/dashboard/components/fast-menu/gpt.tsx
+++ app/dashboard/components/fast-menu/gpt.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Gpt() {
-  return <div>Gpt</div>;
+  return (
+    <div className="flex justify-center font-bold items-center w-full h-full mt-5">
+      Comming soon
+    </div>
+  );
 }
 
 export default Gpt;
